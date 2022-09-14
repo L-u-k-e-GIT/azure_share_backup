25599564c068ebed973f95500099caccab2392b0
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-09-14 14:46:39 +0200

    Add files via upload

---
+++ share_backup.tf
@@ -0,0 +1,8 @@
+
+resource "azurerm_backup_protected_file_share" "share1" {
+  resource_group_name       = var.MD_RG_NAME
+  recovery_vault_name       = var.MD_RECOVERY_VAULT_NAME
+  source_storage_account_id = var.MD_STORAGE_ACCOUNT_ID
+  source_file_share_name    = var.MD_SHARE_NAME
+  backup_policy_id          = var.MD_BCK_POLICY
+}
---
+++ variables.tf
@@ -0,0 +1,36 @@
+
+
+variable "MD_RG_NAME" { 
+ description = "Resource Group Name"
+ type = string
+ default = ""
+}
+
+
+variable "MD_RECOVERY_VAULT_NAME" { 
+ description = "Recovery Vault Name"
+ type = string
+ default = ""
+}
+
+variable "MD_BCK_POLICY" { 
+ description = "backup policy id"
+ type = string
+ default = ""
+}
+
+variable "MD_STORAGE_ACCOUNT_ID" { 
+ description = "Storage account id where share lives"
+ type = string
+ default = ""
+}
+
+
+variable "MD_SHARE_NAME" { 
+ description = "Share to backup"
+ type = string
+ default = ""
+}
+
+
+
