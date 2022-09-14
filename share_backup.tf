8b90f369f1cd6e23c7df599b23897d228867f8d5
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-09-14 17:40:13 +0200

    Update share_backup.tf

--- share_backup.tf
+++ share_backup.tf
@@ -5,5 +5,5 @@ resource "azurerm_backup_protected_file_share" "share1" {
   source_storage_account_id = var.MD_STORAGE_ACCOUNT_ID
   source_file_share_name    = var.MD_SHARE_NAME
   backup_policy_id          = var.MD_BCK_POLICY
-  tags = var.MD_ALL_TAGS
+  
 }
